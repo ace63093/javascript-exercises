ad7976c48cf3680ab8e72bd8f46235ebc5b82bf2
Asadullah <masad321@gmail.com>
2020-04-20 12:20:49 -0700

    Spec Passed

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str,iterations) {
+let ans=''
 
+if (iterations <0){
+return "ERROR"
 }
+else for(let i=0;i<iterations;i++){
+    ans+=str
+}
+
+return ans
+}
+
 
 module.exports = repeatString
