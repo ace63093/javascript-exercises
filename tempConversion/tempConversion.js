055498f72884dc6442bf938e3256f038aaee449f
Asadullah <masad321@gmail.com>
2020-04-20 22:11:42 -0700

    specs passed

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(farenheight) {
+temp = (farenheight-32)/(9/5)
+rtemp=Math.round(temp*10)/10
+return rtemp
 }
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+let temp = (celcius*(9/5))+32;
+rtemp=Math.round(temp*10)/10
+return rtemp 
 }
+
+console.log(ftoc(27))
+console.log(ctof(93))
 
 module.exports = {
   ftoc,
