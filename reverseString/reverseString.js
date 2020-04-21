fa9da97c09416e274fb3bf623292933bd411ea11
Asadullah <masad321@gmail.com>
2020-04-20 17:48:44 -0700

    Spec Passed

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    length=string.length
+    let backwards=['']
+    for (i=length;i>-1;i--){
+        backwards.push(string.slice(i-1,i))
+    }
+backwards=backwards.join('')
+return backwards
 
 }
+
 
 module.exports = reverseString
